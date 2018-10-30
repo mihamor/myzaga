6beabfe8e870347cce998ff0893dae31aa70f3a2
mihamor <mishamoroz5@gmail.com>
2018-10-31 00:01:51 +0200

    changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
 
-    mongo_login: heroku_ggxqz9r1,
-    mongo_password: heroku_ggxqz9r1,
+    mongo_login: "heroku_ggxqz9r1",
+    mongo_password: "heroku_ggxqz9r1",
     mongo_url: "mongodb://heroku_ggxqz9r1:heroku_ggxqz9r1@ds147033.mlab.com:47033/heroku_ggxqz9r1"
 }
 
