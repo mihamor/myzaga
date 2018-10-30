efe92f94d4018ac4bc6f43665ec2615c65b95b1e
mihamor <mishamoroz5@gmail.com>
2018-10-31 00:10:51 +0200

    changes

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
 
     mongo_login: "heroku_ggxqz9r1",
-    mongo_password: "etojemiha",
-    mongo_url: "mongodb://heroku_ggxqz9r1:etojemiha@ds147033.mlab.com:47033/heroku_ggxqz9r1"
+    mongo_password: "kp73miha0471",
+    mongo_url: "mongodb://heroku_ggxqz9r1:kp73miha0471@ds147033.mlab.com:47033/heroku_ggxqz9r1"
 }
 
 module.exports = config;
