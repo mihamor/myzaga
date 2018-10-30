769461ad6a99d9ec9481e8ea09a4885bd74ae138
mihamor <mishamoroz5@gmail.com>
2018-10-31 00:18:48 +0200

    changes

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
 
-    mongo_login: "heroku_ggxqz9r1",
-    mongo_password: "kp73miha0471",
-    mongo_url: "mongodb://heroku_ggxqz9r1:kp73miha0471@ds147033.mlab.com:47033/heroku_ggxqz9r1"
+    mongo_login: "etojemiha",
+    mongo_password: "admin123",
+    mongo_url: "mongodb://etojemiha:admin123@ds147033.mlab.com:47033/heroku_ggxqz9r1"
 }
 
 module.exports = config;
