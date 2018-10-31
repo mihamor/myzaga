2f88d33d56606a4b83a6efbd4eadf4ce5fead4af
mihamor <mishamoroz5@gmail.com>
2018-10-31 18:00:38 +0200

    added mongodb uri

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 let config = {
 
     port : process.env["PORT"] || 3015,
-    mongo_login: "etojemiha",
-    mongo_password: "admin123",
-    mongo_url: "mongodb://etojemiha:admin123@ds147033.mlab.com:47033/heroku_ggxqz9r1"
+    //mongo_url: "mongodb://etojemiha:admin123@ds147033.mlab.com:47033/heroku_ggxqz9r1"
+    mongo_url: provess.env["MONGODB_URI"] || "mongodb://localhost:27017/myzaga"
 }
 
 module.exports = config;
