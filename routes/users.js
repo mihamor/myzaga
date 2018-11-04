5c5c65a36bf8ef07cc25ba70563a97a28a9c4c1e
mihamor <mishamoroz5@gmail.com>
2018-11-04 03:06:57 +0200

    user crud

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {User} = require("../models/user.js");
+const {Utils} = require("../models/utils.js");
 
 const router = express.Router();
 User.setStoragePath("./data/users.json");
@@ -12,6 +13,38 @@ router.get("/", function(req, res){
             req.next();
         });
 });
+
+
+router.get("/new", function(req, res){
+    res.render('users_new');
+});
+
+router.post("/new", function(req, res){
+    let login = req.body.login;
+    let name = req.body.name;
+    let ava_bin = req.files.image;
+    let bio = req.body.bio;
+
+    if(!login || !name 
+    || !ava_bin || !bio ){
+        res.sendStatus(400);
+        return;
+    }
+
+
+    Utils.uploadBufferAsync(ava_bin.data)
+    .then(result => {
+        let user = new User(login, name, 0, result.url, bio);
+        return user;
+    })
+    .then(user => Utils.insertUserWithPlaylist(user))
+    .then(newId => res.redirect(`/users/${newId}`))
+    .catch(err => {
+        console.log(err.message);
+        req.next();
+    })
+});
+
 router.get("/:id", function(req, res){
     let id = req.params.id;
     console.log(id);
@@ -26,5 +59,50 @@ router.get("/:id", function(req, res){
             req.next()
         });
 });
+
+
+router.get("/:id/update", function(req, res){
+    let id = req.params.id;
+
+    User.getById(id)
+        .then(user => res.render('users_update', user))
+        .catch(err => {
+            console.log(err.message);
+            req.next();
+        })
+});
+
+
+router.post("/:id/update", function(req, res){
+    let name = req.body.name;
+    let bio = req.body.bio;
+    let id = req.params.id;
+
+    if(!name || !bio ){
+        res.sendStatus(400);
+        return;
+    }
+
+
+    User.getById(id)
+    .then(user => {
+        if(!user)
+            return Promise.reject("No such user");
+
+        user.bio = bio;
+        user.fullname = name;
+        return user;
+    })
+    .then(user => User.update(user))
+    .then(() => res.redirect(`/users/${id}`))
+    .catch(err => {
+        console.log(err.message);
+        req.next();
+    })
+});
+
+
+
+
 
 module.exports = router;
