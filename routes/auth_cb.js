087d45817f6129c047d447c47a8be5461404fff1
Mykhailo <35976276+mihamor@users.noreply.github.com>
2019-03-16 15:02:27 +0200

    Update auth_cb.js

--- routes/auth_cb.js
+++ routes/auth_cb.js
@@ -3,29 +3,29 @@ const passport = require('passport');
 
 function checkAuth(req, res, next) {
     if (!req.user) return res.sendStatus(401); // 'Not authorized'
-    next();  // пропускати далі тільки аутентифікованих
+    next();
 }
 
 function checkAuthApi(req, res, next) {
     if (!req.user) res.status(403).json({ err: "Unathorized"}); // 'Forbidden'
-    else next();  // пропускати далі тільки аутентифікованих із роллю 'admin'
+    else next();
 }
 
 function checkAuthRedirect(req, res, next) {
     if (!req.user) return res.redirect("/auth/login"); // 'Not authorized'
-    next();  // пропускати далі тільки аутентифікованих
+    next();
 }
 
 
 
 function checkAdmin(req, res, next) {
     if (req.user.role !== 1) res.sendStatus(403); // 'Forbidden'
-    else next();  // пропускати далі тільки аутентифікованих із роллю 'admin'
+    else next();
 }
 
 function checkAdminApi(req, res, next) {
     if (req.user.role !== 1) res.status(403).json({ err: "Forbidden"}); // 'Forbidden'
-    else next();  // пропускати далі тільки аутентифікованих із роллю 'admin'
+    else next();
 }
 let authJWT = passport.authenticate('jwt', {session: false});
 module.exports = {checkAdmin, checkAuth, checkAuthApi, checkAuthRedirect, checkAdminApi, authJWT};
